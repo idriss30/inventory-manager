1cddca8cab43385ba48ffe624ce4f3697359bb0c
idriss30 <idriss.cissoko@hotmail.com>
2022-05-01 14:05:40 -0400

    stored previously added inventory in localStorage

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
-const { handleAddItem, checkFormValues } = require("./domController");
+const {
+  handleAddItem,
+  checkFormValues,
+  updateListItem,
+} = require("./domController");
+const { data } = require("./inventoryController");
 
 const addItemForm = document.getElementById("add-item-form");
 addItemForm.addEventListener("submit", handleAddItem);
 addItemForm.addEventListener("input", checkFormValues);
+
+const myStoredInventory = JSON.parse(localStorage.getItem("inventory"));
+if (myStoredInventory) {
+  data.inventory = myStoredInventory;
+  updateListItem(data.inventory);
+}
