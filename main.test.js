9ea66e246223963e7f17db13d941f93aacff19cc
idriss30 <idriss.cissoko@hotmail.com>
2022-05-01 16:45:27 -0400

    session persistence integration test

--- main.test.js
+++ main.test.js
@@ -9,18 +9,24 @@ beforeEach(() => {
   require("./main"); // attach event to the form everytime body changes
 });
 
-test("add item with form", () => {
-  //replace input values with following
-  screen.getByPlaceholderText("item name").value = "croissant";
-  screen.getByPlaceholderText("item quantity").value = "2";
-
-  const addItemForm = document.querySelector("#add-item-form");
-  fireEvent.submit(addItemForm);
-  const itemList = document.getElementById("list_items");
-  expect(getByText(itemList, `croissant, quantity: 2`)).toBeInTheDocument();
+beforeEach(() => {
+  localStorage.clear();
 });
 
-describe("handle Item name function", () => {
+describe("testing form", () => {
+  test("add item with form", () => {
+    //replace input values with following
+    screen.getByPlaceholderText("item name").value = "croissant";
+    screen.getByPlaceholderText("item quantity").value = "2";
+
+    const addItemForm = document.querySelector("#add-item-form");
+    fireEvent.submit(addItemForm);
+    const itemList = document.getElementById("list_items");
+    expect(getByText(itemList, `croissant, quantity: 2`)).toBeInTheDocument();
+  });
+});
+
+describe("handle Add items function", () => {
   test("entering available items", () => {
     const inputField = screen.getByPlaceholderText("item name");
     inputField.value = "cheesecake";
@@ -33,5 +39,41 @@ describe("handle Item name function", () => {
     const input = screen.getByPlaceholderText("item name");
     fireEvent.input(input, { target: { value: "chocolate" }, bubbles: true });
     expect(screen.getByText("chocolate is not valid")).toBeInTheDocument();
+  });
+});
+
+describe("sessions persistence", () => {
+  beforeEach(() => {
+    localStorage.clear();
+  });
+  test("checking persistence after form submit", () => {
+    const itemNameField = screen.getByPlaceholderText("item name");
+    fireEvent.input(itemNameField, {
+      target: { value: "cheesecake" },
+      bubbles: true,
+    });
+
+    const itemQtyField = screen.getByPlaceholderText("item quantity");
+    fireEvent.input(itemQtyField, { target: { value: "6" }, bubbles: true });
+
+    const submitBtn = screen.getByText("Add item to inventory");
+    fireEvent.click(submitBtn);
+
+    const itemListBefore = document.getElementById("list_items");
+    expect(itemListBefore.childNodes).toHaveLength(1);
+    expect(
+      getByText(itemListBefore, "cheesecake, quantity: 6")
+    ).toBeInTheDocument();
+
+    // This is equivalent to reloading the page
+    document.body.innerHTML = initialHtml;
+    jest.resetModules();
+    require("./main");
+
+    const itemListAfter = document.getElementById("list_items");
+    expect(itemListAfter.childNodes).toHaveLength(1);
+    expect(
+      getByText(itemListAfter, "cheesecake, quantity: 6")
+    ).toBeInTheDocument();
   });
 });
