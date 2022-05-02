37a384ffc7bc35be974a175972484f890fc5dafb
idriss30 <idriss.cissoko@hotmail.com>
2022-05-02 04:06:51 -0400

    adding event listener to undo btn, and popstate to window

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const {
   handleAddItem,
   checkFormValues,
   updateListItem,
+  handleUndone,
+  handlePopState,
 } = require("./domController");
 const { data } = require("./inventoryController");
 
@@ -9,6 +11,10 @@ const addItemForm = document.getElementById("add-item-form");
 addItemForm.addEventListener("submit", handleAddItem);
 addItemForm.addEventListener("input", checkFormValues);
 
+const undoBtn = document.getElementById("undo-btn");
+undoBtn.addEventListener("click", handleUndone);
+
+window.addEventListener("popstate", handlePopState);
 const myStoredInventory = JSON.parse(localStorage.getItem("inventory"));
 if (myStoredInventory) {
   data.inventory = myStoredInventory;
