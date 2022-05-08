a262103311f17c7bae28d01d204282adb023b967
idriss30 <idriss.cissoko@hotmail.com>
2022-05-08 14:35:19 -0400

    refactor addItem to post to inventory route on backend

--- inventoryController.js
+++ inventoryController.js
@@ -1,11 +1,24 @@
 const data = { inventory: {} };
 
-const addItem = (itemName, quantity) => {
+const API_ADDR = "http://localhost:3000";
+const addItem = async (itemName, quantity) => {
   const currentQty = data.inventory[itemName] || 0;
   data.inventory[itemName] = quantity + currentQty;
+  try {
+    await fetch(`${API_ADDR}/inventory/${itemName}`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ itemName, quantity }),
+    });
+  } catch (error) {
+    return error;
+  }
+
+  return data.inventory;
 };
 
 module.exports = {
   addItem,
   data,
+  API_ADDR,
 };
